b14c7f937f34fbcb1c70963a7cb12d81a675d864
ahofacker <andreas.hofacker@gmail.com>
2019-09-02 16:24:22 +0200

    Go to fullscreen, separate file for UI widgets and start networking implementation

--- sample_maker.py
+++ sample_maker.py
@@ -6,156 +6,45 @@ and send it to the "sequencing computer" for playing.
 """
 
 import pygame
-import pygame.gfxdraw
+from ui_widgets import TransportWidget
 
-class TransportWidget:
-    """
-    Widget containing the controls for selecting channels
-    and starting/stopping the recording.
-    """
-    def __init__(self, x, y, w, h):
-        # graphics stuff
-        self.bounding_rect = pygame.Rect(x, y, w, h)
-        self.row_spacing = 100
-
-        # source buttons
-        self.source_button_width = int(0.8 * w / 4.)
-        self.source_button_height = 40
-        self.source_button_spacing = w // 4
-        self.source_button_rects = [pygame.Rect(x + (self.source_button_spacing - self.source_button_width)//2 + i*self.source_button_spacing, y, self.source_button_width, self.source_button_height)
-                                for i in range(4)]
-        self.source_labels = ["Baum", "Harfe", "Gedengel", "Mic"]
-
-        # sync buttons
-        self.sync_button_width = int(0.85 * w / 3.)
-        self.sync_button_height = 40
-        self.sync_button_spacing = w // 3
-        self.sync_button_rects = [pygame.Rect(x + (self.sync_button_spacing - self.sync_button_width)//2 + i*self.sync_button_spacing, y + self.row_spacing, self.sync_button_width, self.sync_button_height)
-                                for i in range(3)]
-        self.sync_labels = ["None", "1 beat", "1 bar"]
-
-        # rec/stop button
-        self.button_width = 100
-        self.icon_width = int(self.button_width * 0.5)
-        self.rec_stop_rect = pygame.Rect(x + w//2 - self.button_width//2, y + 2 * self.row_spacing, self.button_width, self.button_width)
-        self.rec_stop_icon_rect = pygame.Rect(x + w//2 - self.icon_width//2, self.rec_stop_rect.centery - self.icon_width//2, self.icon_width, self.icon_width)
-
-        # interaction stuff
-        self.rec_state = 'stopped'
-        self.sync_state = 2
-        self.source_state = 0
-
-    def handle_click(self, mousepos):
-        if self.rec_stop_rect.collidepoint(mousepos):
-            if self.rec_state == 'stopped':
-                self.rec_state = 'recording'
-            else:
-                self.rec_state = 'stopped'
-        for i, button_rect in enumerate(self.sync_button_rects):
-            if button_rect.collidepoint(mousepos):
-                self.sync_state = i
-        for i, button_rect in enumerate(self.source_button_rects):
-            if button_rect.collidepoint(mousepos):
-                self.source_state = i
-
-    def draw_labels(self, screen):
-        """ Draw the labels of the sync switch buttons on the screen """
-        font = pygame.font.SysFont('Arial', 20)
-
-        for i, label in enumerate(self.source_labels):
-            if self.source_state == i:
-                bgcol = (0, 0, 255)
-            else:
-                bgcol = (0, 0, 0)
-            text_surface = font.render(label, True, (255, 255, 255, 255), bgcol)
-            textrect = text_surface.get_rect()
-            textrect.centerx = self.source_button_rects[i].x + self.source_button_width/2
-            textrect.centery = self.source_button_rects[i].y + self.source_button_height/2
-
-            screen.blit(text_surface, textrect)
-
-        for i, label in enumerate(self.sync_labels):
-            if self.sync_state == i:
-                bgcol = (0, 255, 0)
-            else:
-                bgcol = (0, 0, 0)
-            text_surface = font.render(label, True, (255, 255, 255, 255), bgcol)
-            textrect = text_surface.get_rect()
-            textrect.centerx = self.sync_button_rects[i].x + self.sync_button_width/2
-            textrect.centery = self.sync_button_rects[i].y + self.sync_button_height/2
-
-            screen.blit(text_surface, textrect)
-
-    def draw(self, screen):
-        screen.fill((0, 0, 0), self.bounding_rect)
-
-        for i, button_rect in enumerate(self.source_button_rects):
-            if self.source_state == i:
-                fill_color = (0, 0, 255)
-            else:
-                fill_color = (0, 0, 0)
-            pygame.draw.rect(screen, fill_color, button_rect)
-            pygame.draw.rect(screen, (255, 255, 255), button_rect, 2)
-
-        for i, button_rect in enumerate(self.sync_button_rects):
-            if self.sync_state == i:
-                fill_color = (0, 255, 0)
-            else:
-                fill_color = (0, 0, 0)
-            pygame.draw.rect(screen, fill_color, button_rect)
-            pygame.draw.rect(screen, (255, 255, 255), button_rect, 2)
-
-        self.draw_labels(screen)
-
-        if self.rec_state == 'stopped':
-            pygame.draw.rect(screen, (255, 255, 255), self.rec_stop_rect, 2)
-            pygame.gfxdraw.filled_circle(screen, self.rec_stop_icon_rect.centerx, self.rec_stop_icon_rect.centery, self.icon_width//2, (255, 0, 0))
-            pygame.gfxdraw.aacircle(screen, self.rec_stop_icon_rect.centerx, self.rec_stop_icon_rect.centery, self.icon_width//2, (255, 0, 0))
-        else:
-            pygame.draw.rect(screen, (255, 0, 0), self.rec_stop_rect)
-            pygame.draw.rect(screen, (255, 255, 255), self.rec_stop_rect, 2)
-            pygame.draw.rect(screen, (255, 255, 255), self.rec_stop_icon_rect)
-
-class SyncSwitchWidget:
-    def __init__(self, x, y, w, h):
-        # graphics stuff
-        self.bounding_rect = pygame.Rect(x, y, w, h)
-
-        self.button_width = int(0.8 * w / 3.)
-        self.button_height = 40
-        self.button_spacing = w // 3
-        self.button_rects = [pygame.Rect(x + (self.button_spacing - self.button_width)//2 + i*self.button_spacing, y, self.button_width, self.button_height)
-                                for i in range(3)]
-
-    def draw(self, screen):
-        screen.fill((0, 0, 0), self.bounding_rect)
-        
-        for button_rect in self.button_rects:
-            pygame.draw.rect(screen, (0, 0, 0), button_rect)
-            pygame.draw.rect(screen, (255, 255, 255), button_rect, 2)
-
+# fetch ip of mqtt broker
+mqtt_broker_ip = input("Type in IP of MQTT broker or press Enter to use 192.168.2.107:")
+if mqtt_broker_ip == "":
+    mqtt_broker_ip = "192.168.2.107"
 
 pygame.init()
-screen = pygame.display.set_mode((600, 400))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("The Sample Maker")
+width, height = screen.get_width(), screen.get_height()
 
+# create the UI widget objects
+transport_widget = TransportWidget(100, 100, width-200, height-200, mqtt_broker_ip=mqtt_broker_ip)
+
+# collect the event handlers
+# call those on clicking the mouse, passing the mouse coordinates as a tuple
+on_mouse_click_functions = [transport_widget.handle_click]
+
+# do initial draw
 screen.fill((0, 0, 0))
-
-transport_widget = TransportWidget(50, 50, 500, 300)
 transport_widget.draw(screen)
-
-pygame.display.flip()
+pygame.display.flip()  # update screen
 
 running = True
 while running:
+    # handle events
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
-            transport_widget.handle_click(pygame.mouse.get_pos())
-        if event.type == pygame.QUIT:
+            for click_handler in on_mouse_click_functions:
+                click_handler(pygame.mouse.get_pos())
+        if (event.type == pygame.QUIT 
+            or (event.type == pygame.KEYDOWN and event.key == pygame.K_q)):
             running = False
 
+    # draw stuff
     transport_widget.draw(screen)
 
+    # update screen
     pygame.display.flip()
 
 pygame.quit()
---
+++ ui_widgets.py
@@ -0,0 +1,136 @@
+import pygame
+import pygame.gfxdraw
+import paho.mqtt.client as mqcl
+import json
+
+class TransportWidget:
+    """
+    Widget containing the controls for selecting channels
+    and starting/stopping the recording.
+    """
+    def __init__(self, x, y, w, h, mqtt_broker_ip="192.168.2.107"):
+        self.name = "sampling_launcher"
+
+        # graphics stuff
+        self.bounding_rect = pygame.Rect(x, y, w, h)
+        self.row_spacing = h // 3
+        self.font_size = 30
+
+        # source buttons
+        self.source_button_width = int(0.8 * w / 4.)
+        self.source_button_height = 80
+        self.source_button_spacing = w // 4
+        self.source_button_rects = [pygame.Rect(x + (self.source_button_spacing - self.source_button_width)//2 + i*self.source_button_spacing, y, self.source_button_width, self.source_button_height)
+                                for i in range(4)]
+        self.source_labels = ["Baum", "Harfe", "Gedengel", "Mic"]
+
+        # sync buttons
+        self.sync_button_width = int(0.85 * w / 3.)
+        self.sync_button_height = 80
+        self.sync_button_spacing = w // 3
+        self.sync_button_rects = [pygame.Rect(x + (self.sync_button_spacing - self.sync_button_width)//2 + i*self.sync_button_spacing, y + self.row_spacing, self.sync_button_width, self.sync_button_height)
+                                for i in range(3)]
+        self.sync_labels = ["None", "1 beat", "1 bar"]
+
+        # rec/stop button
+        self.button_width = 200
+        self.icon_width = int(self.button_width * 0.5)
+        self.rec_stop_rect = pygame.Rect(x + w//2 - self.button_width//2, y + 2 * self.row_spacing, self.button_width, self.button_width)
+        self.rec_stop_icon_rect = pygame.Rect(x + w//2 - self.icon_width//2, self.rec_stop_rect.centery - self.icon_width//2, self.icon_width, self.icon_width)
+
+        # interaction stuff
+        self.rec_state = 0  # 0: stopped, 1: record pressed
+        self.sync_state = 2
+        self.source_state = 0
+
+        # networking stuff
+        self.mqtt_client = mqcl.Client(client_id=self.name, clean_session=True)
+        self.mqtt_client.on_message = self.on_mqtt_message
+
+        self.mqtt_client.connect(mqtt_broker_ip, 1883, 60)
+        self.mqtt_client.subscribe("timing/beats", qos=0)
+        self.mqtt_client.loop_start()
+
+    def on_mqtt_message(self, client, userdata, msg):
+        pass
+
+    def send_state(self):
+        payload = json.dumps({'sender_id': self.name, 
+                            'state': {
+                                'source': self.source_state,
+                                'sync': self.sync_state,
+                                'record_pressed': self.rec_state
+                            }})
+        self.mqtt_client.publish("sampling", payload, qos=1, retain=True)
+
+    def handle_click(self, mousepos):
+        if self.rec_stop_rect.collidepoint(mousepos):
+            if self.rec_state == 0:  # if stopped
+                self.rec_state = 1  # set record button to pressed
+            else:
+                self.rec_state = 0
+        for i, button_rect in enumerate(self.sync_button_rects):
+            if button_rect.collidepoint(mousepos):
+                self.sync_state = i
+        for i, button_rect in enumerate(self.source_button_rects):
+            if button_rect.collidepoint(mousepos):
+                self.source_state = i
+
+    def draw_labels(self, screen):
+        """ Draw the labels of the sync switch buttons on the screen """
+        font = pygame.font.SysFont('Arial', self.font_size)
+
+        for i, label in enumerate(self.source_labels):
+            if self.source_state == i:
+                bgcol = (0, 0, 255)
+            else:
+                bgcol = (0, 0, 0)
+            text_surface = font.render(label, True, (255, 255, 255, 255), bgcol)
+            textrect = text_surface.get_rect()
+            textrect.centerx = self.source_button_rects[i].x + self.source_button_width/2
+            textrect.centery = self.source_button_rects[i].y + self.source_button_height/2
+
+            screen.blit(text_surface, textrect)
+
+        for i, label in enumerate(self.sync_labels):
+            if self.sync_state == i:
+                bgcol = (0, 255, 0)
+            else:
+                bgcol = (0, 0, 0)
+            text_surface = font.render(label, True, (255, 255, 255, 255), bgcol)
+            textrect = text_surface.get_rect()
+            textrect.centerx = self.sync_button_rects[i].x + self.sync_button_width/2
+            textrect.centery = self.sync_button_rects[i].y + self.sync_button_height/2
+
+            screen.blit(text_surface, textrect)
+
+    def draw(self, screen):
+        screen.fill((0, 0, 0), self.bounding_rect)
+
+        for i, button_rect in enumerate(self.source_button_rects):
+            if self.source_state == i:
+                fill_color = (0, 0, 255)
+            else:
+                fill_color = (0, 0, 0)
+            pygame.draw.rect(screen, fill_color, button_rect)
+            pygame.draw.rect(screen, (255, 255, 255), button_rect, 2)
+
+        for i, button_rect in enumerate(self.sync_button_rects):
+            if self.sync_state == i:
+                fill_color = (0, 255, 0)
+            else:
+                fill_color = (0, 0, 0)
+            pygame.draw.rect(screen, fill_color, button_rect)
+            pygame.draw.rect(screen, (255, 255, 255), button_rect, 2)
+
+        self.draw_labels(screen)
+
+        if self.rec_state == 0:  # "stopped" state
+            pygame.draw.rect(screen, (0, 0, 0), self.rec_stop_rect)
+            pygame.draw.rect(screen, (255, 255, 255), self.rec_stop_rect, 2)
+            pygame.gfxdraw.filled_circle(screen, self.rec_stop_icon_rect.centerx, self.rec_stop_icon_rect.centery, self.icon_width//2, (255, 0, 0))
+            pygame.gfxdraw.aacircle(screen, self.rec_stop_icon_rect.centerx, self.rec_stop_icon_rect.centery, self.icon_width//2, (255, 0, 0))
+        else:
+            pygame.draw.rect(screen, (255, 0, 0), self.rec_stop_rect)
+            pygame.draw.rect(screen, (255, 255, 255), self.rec_stop_rect, 2)
+            pygame.draw.rect(screen, (255, 255, 255), self.rec_stop_icon_rect)
